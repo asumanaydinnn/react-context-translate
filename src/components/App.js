6f43704033e34064e250f41a7840744606600194
Asuman Aydın <asuman.aydin6709@gmail.com>
2022-04-17 23:29:23 +0300

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+
+class App extends React.Component {
+  state = { language: 'english' };
+  onLanguageChange = language => {
+    this.setState({ language: language });
+  };
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i className="flag us" onClick={() => this.onLanguageChange('english')} />
+          <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
+        </div>
+        {this.state.language}
+        <LanguageContext.Provider value={this.state.language}>
+          <UserCreate />
+        </LanguageContext.Provider>
+      </div>
+    );
+  }
+}
+
+export default App;
